ea65c147692face3aa231e3ce0a312d8c9142e9d
Lucas <lucass@gmail.com>
2014-06-15 16:34:41 -0300

    Loading page on webview

--- src/com/github/beothorn/JogodegoActivity.java
+++ src/com/github/beothorn/JogodegoActivity.java
@@ -2,14 +2,16 @@ package com.github.beothorn;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
-public class JogodegoActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class JogodegoActivity extends Activity
+{
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        WebView mWebView=(WebView)findViewById(R.id.webView);
+        mWebView.loadUrl("file:///android_asset/index.html");
     }
 }
